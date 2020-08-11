8cd0a363309bbc093798bfafe84efbb8458bbd26
Just Maks <just.maks.bro@gmail.com>
2020-08-11 16:07:15 +0300

    Implement InitDirect function

--- DirectX9/Source.cpp
+++ DirectX9/Source.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 #include <stdint.h>
+#include <d3dx9.h>
+#include <d3d9.h>
 
 // TYPE DEFINITIONS
 typedef int32_t int32;
@@ -17,13 +19,77 @@ typedef uint8_t  uint8;
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "Gdi32.lib")
 
-//- 
-#define global_var static
 
-//GLOBAL VARIABLES
-HWND hWnd_g = nullptr; // window handler 
-uint32 screenWidth_g = 600; //window width
-uint32 screenHeight_g = 600; // width height
+//GLOBAL  COMMON VARIABLES
+HWND g_hWnd = nullptr; // window handler 
+uint32 g_ScreenWidth = 600; //window width
+uint32 g_ScreenHeight = 600; // width height
+
+// DELETE DIRECT3D OBJECTS 
+#define RELEASE(obj) if(obj != nullptr){obj->Release();obj = nullptr;}
+//GLOBAL DIRECT3D VARIABLES
+static IDirect3DDevice9*  g_Device = nullptr;
+ 
+bool InitDirect3D()
+{
+	// create Direct object
+	IDirect3D9* directObj = Direct3DCreate9(D3D_SDK_VERSION);
+
+	//check capabilities
+	D3DCAPS9 devCaps{}; 
+	directObj->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &devCaps); 
+	bool supportsHardwareVertexProcessing = devCaps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT;
+
+	// set paramaters
+	D3DPRESENT_PARAMETERS param{};
+	param.Windowed = TRUE; 
+	param.SwapEffect = D3DSWAPEFFECT_DISCARD;  
+	param.hDeviceWindow = g_hWnd; 
+	param.EnableAutoDepthStencil = TRUE; // z-buffer
+	param.AutoDepthStencilFormat = D3DFMT_A8R8G8B8; // z-buffer
+
+	//create device
+	DWORD vertexProcessing = supportsHardwareVertexProcessing?  D3DCREATE_HARDWARE_VERTEXPROCESSING : D3DCREATE_SOFTWARE_VERTEXPROCESSING; 
+	directObj->CreateDevice(D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		param.hDeviceWindow,
+		vertexProcessing,
+		&param,
+		&g_Device); 
+
+	// release Direct object
+	RELEASE(directObj)
+
+	
+}
+
+// CREATE VERTEX
+
+// SETUP DIRECT3D ENVIRONMENT
+	/* 
+	-> SETUP  VIEW TRANSFORMATION
+	-> SETUP  PROJECTION TRANSFORMATION 
+	-> 
+	*/
+
+//SETUP GRAPHIC OBJECTS 
+	/*
+	-> create and fill vertex buffer
+	-> create teapot ojb
+	*/
+
+// DRAW FUNCTION
+	/*
+	-> CLEAN BUFFER
+	-> SET WORLD MATRIX
+	-> DRAW OBJECTS
+	*/
+
+
+void ReleaseDirectObjects()
+{
+	// usign to free Direct obj
+}
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -68,10 +134,10 @@ bool InitWindow(uint32 width, uint32 height)
 
 
 	//CREATE WINDOW
-	hWnd_g = CreateWindow(
+	g_hWnd= CreateWindow(
 		L"Direct3D9App", 
 		L"Direct3D9App",
-		WS_EX_TOPMOST|WS_SYSMENU,
+		WS_EX_TOPMOST|WS_SYSMENU|WS_SIZEBOX, 
 		0, 0,
 		width, height,
 		0 /*parent hwnd*/, 
@@ -79,13 +145,13 @@ bool InitWindow(uint32 width, uint32 height)
 		GetModuleHandle(NULL), 
 		0 /*extra*/);
 
-	if (!hWnd_g)
+	if (!g_hWnd)
 	{
 		::MessageBox(0, L"CreateWindow() - FAILED", 0, 0);
 		return false;
 	}
 
-	ShowWindow(hWnd_g, SW_SHOW);
+	ShowWindow(g_hWnd, SW_SHOW);
 	//END CREATE WINDOW
 
 	return true; 
@@ -99,7 +165,7 @@ int WINAPI wWinMain(HINSTANCE hinstance,
 					int showCmd)
 {
 	
-	if (InitWindow(screenWidth_g, screenHeight_g))
+	if (InitWindow(g_ScreenWidth, g_ScreenHeight))
 	{
 		MSG msg{};
 		//GAME LOOP 
